f40d6a163414e795370018acb593a9385c4791cb
Yechiel K <chilikkal@gmail.com>
2016-12-14 20:30:31 -0500

    added morningRoutine that returns a function

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function exerciseRoutine(postRunActivity) {
   runFiveMiles();
   postRunActivity();
 }
+
+function morningRoutine(exercise) {
+  var breakfast = null;
+
+  if (exercise === liftWeights) {
+    breakfast = 'protein bar';
+  } else if (exercise === swimFortyLaps) {
+    breakfast = 'kale smoothie';
+  } else {
+    breakfast = 'granola';
+  }
+
+  exerciseRoutine(exercise);
+
+
+  return function() {
+    console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+  }
+}
